8d43cc0d6ccf5543205e6ea4e4b47fd40cb2799e
lakshitha212 <chameera.lakshitha212@gmail.com>
2016-01-08 13:39:11 +0530

    update d3.js

--- js/d3-slider.js
+++ js/d3-slider.js
@@ -10,11 +10,12 @@
 
         var url,
             colorRange,
+            chartType,
             settings = $.extend({
                 url: 'data.json',
-                colorRange: ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"]
+                colorRange: ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"],
+                chartType: [[1, 'Single or Grouped Bar chart'], [2, 'Pie Chart'], [3, 'Donut Chart'], [4, 'Horizontal Single or Multiple Bar Chart'], [5, 'Zoomable Sunburst Chart']]
             }, options);
-        console.log(settings);
 
         /**
          * define json file path
@@ -35,7 +36,17 @@
          */
         $.getJSON(url, function () {
         }).done(function (json) {
-            console.log(json);
+            if (json.length > 0) {
+                for (var i in json) {
+                    if (i in json) {
+                        Object.keys(json[i]).forEach(function (key) {
+                            if (key in json[i]) {
+                                console.log(json[i][key]);
+                            }
+                        });
+                    }
+                }
+            }
         }).fail(function (jqxhr, textStatus, error) {
             var err = textStatus + ", " + error;
             console.log("Request Failed: " + err);
